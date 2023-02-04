9bb9a829e87a03cddd68ae258173648359f343bd
arwa <arwashams643@gmail.com>
2023-02-04 23:02:36 +0200

    create Database

---
+++ app/src/main/java/com/example/foodplannerapp/database/MyRoomDataBase.java
@@ -0,0 +1,39 @@
+package com.example.foodplannerapp.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+
+import com.example.foodplannerapp.models.MealModel;
+import com.example.foodplannerapp.utils.Constants;
+
+@Database(entities = {MealModel.class}, version = 1, exportSchema = false)
+@TypeConverters({Converters.class})
+public abstract class MyRoomDataBase extends RoomDatabase {
+
+    private static MyRoomDataBase myRoomDataBase = null;
+
+    public abstract MealDao getDao();
+
+    public static synchronized MyRoomDataBase initRoom(Context context) {
+        if (myRoomDataBase == null) {
+            myRoomDataBase = Room.
+                    databaseBuilder(context, MyRoomDataBase.class, Constants.MEAL_DATABASE)
+                    .fallbackToDestructiveMigration()
+                    .allowMainThreadQueries()
+                    .build();
+        }
+
+        return myRoomDataBase;
+
+    }
+
+    public MyRoomDataBase getInstance(){
+        return  myRoomDataBase;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/foodplannerapp/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.example.foodplannerapp.utils;
+
+public class Constants {
+
+    public static final String TABLE_NAME = "MEAL_TABLE" ;
+    public static final String MEAL_DATABASE = "MEAL_DataBase";
+}
