dacaaeba8e2ff8efc6e3e1733246821d1ae9c02e
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-02-04 23:31:33 +0200

    Upload Search Design Screen

---
+++ app/src/main/java/com/example/foodplannerapp/SearchFragment.java
@@ -0,0 +1,27 @@
+package com.example.foodplannerapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SearchFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
