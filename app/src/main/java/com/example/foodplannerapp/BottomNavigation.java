d2142cec4eaacc12d7aafe798008bc0dc2719ce9
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-02-04 23:27:43 +0200

    Upload navigation Bottom

---
+++ app/src/main/java/com/example/foodplannerapp/BottomNavigation.java
@@ -0,0 +1,24 @@
+package com.example.foodplannerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class BottomNavigation extends AppCompatActivity {
+    BottomNavigationView bottom_navigation;
+    NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation);
+        bottom_navigation=findViewById(R.id.bottom_navigation);
+        navController= Navigation.findNavController(this,R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottom_navigation,navController);
+    }
+}
