aee0260ea227f28f9e52b52990e05fee9fd7b7da
arwa <arwashams643@gmail.com>
2023-02-05 15:38:24 +0200

    Handle the rest of navigation

--- app/src/main/java/com/example/foodplannerapp/HomeFragment.java
+++ app/src/main/java/com/example/foodplannerapp/HomeFragment.java
@@ -2,26 +2,50 @@ package com.example.foodplannerapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.foodplannerapp.databinding.FragmentHomeBinding;
+
 
 public class HomeFragment extends Fragment {
 
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
+    private FragmentHomeBinding binding;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        onClicks();
+
+    }
+
+    private void onClicks() {
+        binding.cardViewMealHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_detailsFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
--- app/src/main/java/com/example/foodplannerapp/ui/welcome/WelcomeFragment.java
+++ app/src/main/java/com/example/foodplannerapp/ui/welcome/WelcomeFragment.java
@@ -1,16 +1,20 @@
 package com.example.foodplannerapp.ui.welcome;
 
+import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.foodplannerapp.BottomNavigation;
 import com.example.foodplannerapp.R;
 import com.example.foodplannerapp.databinding.FragmentWelcomeBinding;
 
@@ -38,11 +42,51 @@ public class WelcomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         CreateLineUnderLogin();
+        onClicks();
 
     }
 
     private void CreateLineUnderLogin() {
         binding.textViewLogin.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+    }
+
+    private void onClicks() {
+        binding.btnFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(requireContext(), "FaceBook is Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        binding.btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(requireContext(), "Google is Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        binding.btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_welcomeFragment_to_signUpFragment);
+            }
+        });
+
+        binding.textViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_welcomeFragment_to_logInFragment);
+            }
+        });
+
+        binding.btnSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), BottomNavigation.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
