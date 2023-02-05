cebb895499ce084ea3e4698500309985876f8688
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-02-05 16:49:26 +0200

    Create CategoriesModel and CategoriesRootModel

---
+++ app/src/main/java/com/example/foodplannerapp/models/CategoriesRootModel.java
@@ -0,0 +1,10 @@
+package com.example.foodplannerapp.models;
+
+import java.util.ArrayList;
+
+public class CategoriesRootModel {
+
+        ArrayList< Object > categories = new ArrayList < Object > ();
+
+
+}
