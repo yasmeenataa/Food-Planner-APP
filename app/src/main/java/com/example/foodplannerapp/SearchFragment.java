7f18149bbb693a958f3886f813941295155253e6
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-02-08 16:56:05 +0200

    update search result

--- app/src/main/java/com/example/foodplannerapp/SearchFragment.java
+++ app/src/main/java/com/example/foodplannerapp/SearchFragment.java
@@ -10,18 +10,56 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.foodplannerapp.databinding.FragmentSearchBinding;
+import com.example.foodplannerapp.models.CategoriesModel;
+import com.example.foodplannerapp.models.MealModel;
+import com.example.foodplannerapp.ui.favourite.FavouriteAdapter;
+
+import java.util.ArrayList;
+
 
 public class SearchFragment extends Fragment {
-
+    private FragmentSearchBinding binding;
+    private ArrayList<CategoriesModel> list;
+    private CategoriesAdapter adapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+
+        binding = FragmentSearchBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
         super.onViewCreated(view, savedInstanceState);
+
+
+        list = new ArrayList<>();
+        inflateRecyclerView();
+
+    }
+
+    private ArrayList<CategoriesModel> fillList() {
+        CategoriesModel categoriesModel = new CategoriesModel();
+        for (int i = 0; i <= 10; i++) {
+            categoriesModel.setStrCategory("Beef" + i);
+            categoriesModel.setStrCategoryThumb("https:\\/\\/www.themealdb.com\\/images\\/category\\/beef.png" + i);
+            list.add(i, categoriesModel);
+        }
+        return list;
+    }
+
+    private void inflateRecyclerView() {
+        adapter=new CategoriesAdapter(fillList(),getContext());
+
+        binding.recyclerSearchCat.setAdapter(adapter);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
