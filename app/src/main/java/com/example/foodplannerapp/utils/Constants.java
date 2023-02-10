fe500eb57fb49524ca29da55079de36e25ce1c64
arwa <arwashams643@gmail.com>
2023-02-10 04:10:31 +0200

    add base url

--- app/src/main/java/com/example/foodplannerapp/utils/Constants.java
+++ app/src/main/java/com/example/foodplannerapp/utils/Constants.java
@@ -2,6 +2,8 @@ package com.example.foodplannerapp.utils;
 
 public class Constants {
 
-    public static final String TABLE_NAME = "MEAL_TABLE" ;
+    public static final String TABLE_NAME = "MEAL_TABLE";
     public static final String MEAL_DATABASE = "MEAL_DataBase";
+
+    public static final String BASE_URL = "https://www.themealdb.com/api/json/v1/1/";
 }
