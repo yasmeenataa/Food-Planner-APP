e3e01f22ca6ef1f7eac38650a88915d50ab4141f
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-02-12 01:16:05 +0200

    handle logic of OnItemClickListener.java

--- app/src/main/java/com/example/foodplannerapp/ui/search/view/OnItemClickListener.java
+++ app/src/main/java/com/example/foodplannerapp/ui/search/view/OnItemClickListener.java
@@ -1,7 +1,7 @@
 package com.example.foodplannerapp.ui.search.view;
 
 public interface OnItemClickListener {
-    void OnIngredientClick();
-    void OnCategoryClick();
-    void OnAreaClick();
+    void OnIngredientClick(String item_name);
+    void OnCategoryClick(String item_name);
+    void OnAreaClick(String item_name);
 }
