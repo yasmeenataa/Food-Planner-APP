5f213e3d288983503d85255a3e1714f8a8f2b147
arwa <arwashams643@gmail.com>
2023-02-12 00:36:21 +0200

    create and imp days of the week

---
+++ app/src/main/java/com/example/foodplannerapp/ui/dayFragment/presenter/DayPresenter.java
@@ -0,0 +1,62 @@
+package com.example.foodplannerapp.ui.dayFragment.presenter;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.foodplannerapp.models.MySharedPref;
+import com.example.foodplannerapp.models.WeekPlannerModel;
+import com.example.foodplannerapp.repo.mealRepo.MealRepo;
+import com.example.foodplannerapp.ui.dayFragment.view.DayViewInterface;
+import com.example.foodplannerapp.utils.Constants;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.CompletableObserver;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class DayPresenter implements DayPresenterInterface {
+
+    private DayViewInterface viewInterface;
+
+    private DatabaseReference reference;
+
+    private MealRepo repo;
+
+
+    public DayPresenter(DayViewInterface viewInterface) {
+        this.viewInterface = viewInterface;
+        repo = MealRepo.getMealRepoInstance();
+        reference = FirebaseDatabase.getInstance().getReference();
+    }
+
+    @Override
+    public void insertMealToDay(WeekPlannerModel weekPlannerModel) {
+        repo.insertWeekMeal(weekPlannerModel).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new CompletableObserver() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        viewInterface.getErrorMessage(e.getMessage());
+                    }
+                });
+    }
+
+    @Override
+    public void insertMealToDayToFirebase(WeekPlannerModel weekPlannerModel) {
+        reference.child(MySharedPref.getUserId())
+                .child(Constants.WEEK_REF)
+                .child(weekPlannerModel.getDay().concat("_").concat(weekPlannerModel.getIdMeal()))
+                .setValue(weekPlannerModel);
+    }
+}
---
+++ app/src/main/java/com/example/foodplannerapp/ui/weekPlanner/view/AdapterMealOfTheDay.java
@@ -0,0 +1,94 @@
+package com.example.foodplannerapp.ui.weekPlanner.view;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.foodplannerapp.R;
+import com.example.foodplannerapp.models.WeekPlannerModel;
+
+import java.util.ArrayList;
+
+public class AdapterMealOfTheDay extends RecyclerView.Adapter<AdapterMealOfTheDay.Holder> {
+
+    private ArrayList<WeekPlannerModel> list;
+
+    private SetOnItemClickListener onItemClickListener;
+
+    public void setOnItemClickListener(SetOnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    public void setList(ArrayList<WeekPlannerModel> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_week_planner_recycler, parent, false);
+        return new Holder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Holder holder, int position) {
+        WeekPlannerModel model = list.get(position);
+        holder.textViewMealName.setText(model.getStrMeal());
+        Glide.with(holder.itemView.getContext())
+                .load(model.getStrMealThumb())
+                .into(holder.mealImageWeekPlanner);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list == null ? 0 : list.size();
+    }
+
+    public class Holder extends RecyclerView.ViewHolder {
+
+        ImageView deleteImage, mealImageWeekPlanner;
+        TextView textViewMealName;
+
+        public Holder(@NonNull View itemView) {
+            super(itemView);
+            deleteImage = itemView.findViewById(R.id.imageDeleteWeekItem);
+            textViewMealName = itemView.findViewById(R.id.text_view_meal_name_inWeekFrag);
+            mealImageWeekPlanner = itemView.findViewById(R.id.image_of_weekPlanner);
+
+            deleteImage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (onItemClickListener != null) {
+                        onItemClickListener.onDeleteClicked(list.get(getLayoutPosition()));
+                    }
+
+                }
+            });
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (onItemClickListener != null) {
+                        onItemClickListener.onItemClicked(list.get(getLayoutPosition()));
+                    }
+                }
+            });
+
+
+        }
+    }
+
+    interface SetOnItemClickListener {
+        void onDeleteClicked(WeekPlannerModel model);
+
+        void onItemClicked(WeekPlannerModel weekPlannerModel);
+    }
+}
