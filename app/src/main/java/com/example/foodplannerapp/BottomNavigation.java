965d7a04467022a11125f2bb7963e2b2bc6790a6
arwa <arwashams643@gmail.com>
2023-02-12 00:38:28 +0200

    make app singl activity

--- app/src/main/java/com/example/foodplannerapp/BottomNavigation.java
+++ app/src/main/java/com/example/foodplannerapp/BottomNavigation.java
@@ -14,17 +14,17 @@ import com.google.firebase.auth.FirebaseAuth;
 public class BottomNavigation extends AppCompatActivity {
 
 
-    private BottomNavigationView bottom_navigation;
-    private NavController navController;
+//    private BottomNavigationView bottom_navigation;
+//    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation);
 
-        bottom_navigation = findViewById(R.id.bottom_navigation);
-        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupWithNavController(bottom_navigation, navController);
+//        bottom_navigation = findViewById(R.id.bottom_navigation);
+//        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        NavigationUI.setupWithNavController(bottom_navigation, navController);
     }
 
 
