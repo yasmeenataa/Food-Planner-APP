46b150ae5e142f6acc4184cd0f695f79f2e38591
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-02-11 15:28:05 +0200

    handle view of search to call ingredients and area

--- app/src/main/java/com/example/foodplannerapp/ui/search/view/SearchFragment.java
+++ app/src/main/java/com/example/foodplannerapp/ui/search/view/SearchFragment.java
@@ -5,24 +5,36 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.foodplannerapp.models.AreaListModel;
+import com.example.foodplannerapp.models.IngredientListModel;
+import com.example.foodplannerapp.repo.mealRepo.MealRepo;
 import com.example.foodplannerapp.ui.home.view.*;
 import com.example.foodplannerapp.databinding.FragmentSearchBinding;
 import com.example.foodplannerapp.models.CategoriesModel;
 import com.example.foodplannerapp.models.ModelMeal;
 import com.example.foodplannerapp.ui.favourite.FavouriteAdapter;
+import com.example.foodplannerapp.ui.search.presenter.SearchPresenter;
+import com.example.foodplannerapp.ui.search.presenter.SearchPresenterInterface;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
-public class SearchFragment extends Fragment {
+public class SearchFragment extends Fragment implements SearchViewInterface,OnItemClickListener {
     private FragmentSearchBinding binding;
     private ArrayList<CategoriesModel> list;
-    private CategoriesAdapter adapter;
+    private CategoriesAdapter categoriesAdapter;
+    private AreaListAdapter areaListAdapter;
+    private IngredientListAdapter ingredientListAdapter;
+    private SearchPresenterInterface presenterInterface;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -36,31 +48,71 @@ public class SearchFragment extends Fragment {
 
         super.onViewCreated(view, savedInstanceState);
 
-
+        areaListAdapter=new AreaListAdapter(new ArrayList<>(),getContext(),this);
+        ingredientListAdapter=new IngredientListAdapter(new ArrayList<>(),getContext(),this);
         list = new ArrayList<>();
-       // inflateRecyclerView();
+        presenterInterface=new SearchPresenter(this, MealRepo.getMealRepoInstance());
+        presenterInterface.getAreaList();
+        presenterInterface.getIngredientList();
+
 
     }
 
-    private ArrayList<CategoriesModel> fillList() {
-        CategoriesModel categoriesModel = new CategoriesModel();
-        for (int i = 0; i <= 10; i++) {
-            categoriesModel.setStrCategory("Beef" + i);
-            categoriesModel.setStrCategoryThumb("https:\\/\\/www.themealdb.com\\/images\\/category\\/beef.png" + i);
-            list.add(i, categoriesModel);
-        }
-        return list;
+
+
+    @Override
+    public void showIngredientList(List<IngredientListModel> ingredientList) {
+        binding.progressBarSearch.setVisibility(View.GONE);
+        System.out.println("//////////////////////Handled/////////////////////"+ingredientList.get(0).getStrIngredient());
+        ingredientListAdapter.setList((ArrayList<IngredientListModel>) ingredientList);
+        binding.recycleIngredients.setAdapter(ingredientListAdapter);
+        ingredientListAdapter.notifyDataSetChanged();
     }
 
-//    private void inflateRecyclerView() {
-//        adapter=new CategoriesAdapter(fillList(),getContext());
-//
-//        binding.recyclerSearchCat.setAdapter(adapter);
-//    }
+    @Override
+    public void showCategoryList(List<CategoriesModel> categoriesList) {
 
+    }
+
+    @Override
+    public void showAreaList(List<AreaListModel> areaList) {
+        areaListAdapter.setList((ArrayList<AreaListModel>) areaList);
+        binding.recycleArea.setAdapter(areaListAdapter);
+        areaListAdapter.notifyDataSetChanged();
+
+    }
+
+    @Override
+    public void onFailureIngredientList(String errorMsg) {
+        System.out.println("Error : "+errorMsg);
+        Toast.makeText(getContext(),"Error : "+errorMsg,Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onFailureAreaList(String errorMsg) {
+        System.out.println("Error : "+errorMsg);
+        Toast.makeText(getContext(),"Error : "+errorMsg,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void OnIngredientClick() {
+
+    }
+
+    @Override
+    public void OnCategoryClick() {
+
+    }
+
+    @Override
+    public void OnAreaClick() {
+
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+
 }
--- app/src/main/java/com/example/foodplannerapp/ui/search/view/SearchViewInterface.java
+++ app/src/main/java/com/example/foodplannerapp/ui/search/view/SearchViewInterface.java
@@ -1,5 +1,6 @@
 package com.example.foodplannerapp.ui.search.view;
 
+import com.example.foodplannerapp.models.AreaListModel;
 import com.example.foodplannerapp.models.CategoriesModel;
 import com.example.foodplannerapp.models.IngredientListModel;
 
@@ -8,4 +9,8 @@ import java.util.List;
 public interface SearchViewInterface {
     void showIngredientList(List<IngredientListModel> ingredientList);
     void showCategoryList(List<CategoriesModel> categoriesList);
+    void showAreaList(List<AreaListModel> areaList);
+    void onFailureIngredientList(String errorMsg);
+    void onFailureAreaList(String errorMsg);
+
 }
