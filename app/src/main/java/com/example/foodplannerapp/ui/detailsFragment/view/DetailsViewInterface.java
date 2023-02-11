7dca0f7e7f5701568351946d9d390683c1d0710b
arwa <arwashams643@gmail.com>
2023-02-12 00:37:27 +0200

    fix Detils Frag

---
+++ app/src/main/java/com/example/foodplannerapp/ui/detailsFragment/view/DetailsViewInterface.java
@@ -0,0 +1,13 @@
+package com.example.foodplannerapp.ui.detailsFragment.view;
+
+import com.example.foodplannerapp.models.ModelMeal;
+
+public interface DetailsViewInterface {
+
+    void onSuccess(ModelMeal modelMeal);
+
+    void insertMealToFav(ModelMeal modelMeal);
+
+
+
+}
