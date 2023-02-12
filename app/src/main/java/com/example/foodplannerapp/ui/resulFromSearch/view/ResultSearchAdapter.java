e8a3d2ab3c3271938ccbd4dea2d0605df92bfea4
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-02-12 05:22:35 +0200

    handle searchMealByName in presenter and View

--- app/src/main/java/com/example/foodplannerapp/ui/resulFromSearch/view/ResultSearchAdapter.java
+++ app/src/main/java/com/example/foodplannerapp/ui/resulFromSearch/view/ResultSearchAdapter.java
@@ -58,15 +58,14 @@ public class ResultSearchAdapter extends RecyclerView.Adapter<ResultSearchAdapte
     public class Holder extends RecyclerView.ViewHolder {
         TextView txtResult_maelName_itemMeal;
         ImageView image_result_itemMeal;
-        RoundedImageView image_calender_result,image_result_fav;
+
 
         public Holder(@NonNull View itemView) {
             super(itemView);
 
             txtResult_maelName_itemMeal = itemView.findViewById(R.id.txtResult_maelName_itemMeal);
             image_result_itemMeal=itemView.findViewById(R.id.image_result_itemMeal);
-            image_calender_result=itemView.findViewById(R.id.image_calender_result);
-            image_result_fav=itemView.findViewById(R.id.image_result_fav);
+
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
