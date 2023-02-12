c4c8af6fbdddf0bb6bd6b54c84c251277c14810d
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-02-12 05:21:06 +0200

    add clickListener for searchMeal

--- app/src/main/java/com/example/foodplannerapp/ui/search/view/OnItemClickListener.java
+++ app/src/main/java/com/example/foodplannerapp/ui/search/view/OnItemClickListener.java
@@ -2,6 +2,7 @@ package com.example.foodplannerapp.ui.search.view;
 
 public interface OnItemClickListener {
     void OnIngredientClick(String item_name);
-    void OnCategoryClick(String item_name);
+
     void OnAreaClick(String item_name);
+    void onSearchedMealClick(String mealId);
 }
